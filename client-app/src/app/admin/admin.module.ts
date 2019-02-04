6bc609c168372c9ffd58770136be8f402ca9892f
PetyRock <pghebrezghi@gmail.com>
2019-02-04 16:29:02 -0600

    Remove unused code

--- client-app/src/app/admin/admin.module.ts
+++ client-app/src/app/admin/admin.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { EvaluationComponent } from './evaluation/evaluation.component';
 import { CreatingExamComponent } from './create-question/creating-question.component';
-import { ManagingStaffComponent } from './managing-staff/managing-staff.component';
 import { routing } from './adminRoutes'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { AdminhomeComponent } from './adminhome/adminhome.component';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   imports: [
@@ -16,16 +12,10 @@ import { AdminLoginComponent } from './admin-login/admin-login.component';
     FormsModule
   ],
   declarations: [
-    EvaluationComponent,
-    CreatingExamComponent,
-   // ManagingStaffComponent,
-    AdminhomeComponent,
-    AdminLoginComponent
+    CreatingExamComponent
   ],
   exports: [
-    EvaluationComponent,
     CreatingExamComponent,
-  //  ManagingStaffComponent
   ]
 })
 export class AdminModule { }
--- client-app/src/app/admin/adminRoutes.ts
+++ client-app/src/app/admin/adminRoutes.ts
@@ -1,13 +1,10 @@
-import { AdminLoginComponent } from "./admin-login/admin-login.component";
+
 import { CreatingExamComponent } from "./create-question/creating-question.component";
-import { EvaluationComponent } from "./evaluation/evaluation.component";
-import { ManagingStaffComponent } from "./managing-staff/managing-staff.component";
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
-import { AdminhomeComponent } from "./adminhome/adminhome.component";
 
 export const routes: Routes = [
-    { path: '', component: AdminhomeComponent },
+    { path: ''},
     {
         path: 'create-question', component: CreatingExamComponent
         /*children: [
