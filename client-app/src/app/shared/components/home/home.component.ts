341f07c48dd70c0841ad023ba22c392f3b497d8c
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-02-06 13:48:09 -0600

    Homepage with userIsAuthenticated

--- client-app/src/app/shared/components/home/home.component.ts
+++ client-app/src/app/shared/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  isAdmin = false;
+  isStaff = false;
+  userIsAuthenticated = false;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.isAdmin = this.authService.isAdmin();
+    this.isStaff = this.authService.isStaff();
+    this.userIsAuthenticated = this.authService.getIsAuth();
   }
 }
