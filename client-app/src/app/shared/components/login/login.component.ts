670c5d92e5acf0b3841f169a6f0d5df57bb996d7
Luk Pheakdey <luk.pheakdey@gmail.com>
2019-02-06 19:52:53 -0600

    login with email need to be valid email format

--- client-app/src/app/shared/components/login/login.component.ts
+++ client-app/src/app/shared/components/login/login.component.ts
@@ -30,6 +30,5 @@ export class LoginComponent implements OnInit {
   onSubmit() {
     const login = { email: this.loginForm.value.email, password: this.loginForm.value.password };
     this.authService.login(login);
-
   }
 }
